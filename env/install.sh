878d802677d5fdea15a55a1405d106b66c3e1986
ayush4computing <47182277+ayush4computing@users.noreply.github.com>
2022-06-26 11:48:46 +0530

    Delete install.sh

--- env/install.sh
+++
@@ -1,9 +0,0 @@
-sed $'s/\r$//' ./install.sh > ./install.Unix.sh
-wget https://packages.microsoft.com/config/ubuntu/21.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-sudo dpkg -i packages-microsoft-prod.deb
-sudo apt-get update; \
-sudo apt-get install -y apt-transport-https && \
-sudo apt-get update && \
-sudo apt-get install -y dotnet-sdk-3.1 && \
-sudo apt-get install -y aspnetcore-runtime-3.1
- 
