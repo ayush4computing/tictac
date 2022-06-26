1f36d31c52d4a56089e46f4a969533f2c33c622b
ayush4computing <47182277+ayush4computing@users.noreply.github.com>
2022-06-26 11:04:11 +0530

    Add files via upload

---
+++ TicTacToe.Data/Migrations/20220611171128_Game1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TicTacToe.Data.Migrations
+{
+    public partial class Game1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GameState",
+                columns: table => new
+                {
+                    GameStateId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TL = table.Column<string>(nullable: false),
+                    TR = table.Column<string>(nullable: false),
+                    TM = table.Column<string>(nullable: false),
+                    ML = table.Column<string>(nullable: false),
+                    MR = table.Column<string>(nullable: false),
+                    MM = table.Column<string>(nullable: false),
+                    BL = table.Column<string>(nullable: false),
+                    BM = table.Column<string>(nullable: false),
+                    BR = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GameState", x => x.GameStateId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Game",
+                columns: table => new
+                {
+                    GameId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PlayerX = table.Column<string>(maxLength: 100, nullable: true),
+                    Player0 = table.Column<string>(maxLength: 100, nullable: true),
+                    GameResult = table.Column<string>(maxLength: 100, nullable: true),
+                    Date = table.Column<DateTime>(nullable: false),
+                    GameStateId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Game", x => x.GameId);
+                    table.ForeignKey(
+                        name: "FK_Game_GameState_GameStateId",
+                        column: x => x.GameStateId,
+                        principalTable: "GameState",
+                        principalColumn: "GameStateId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Game_GameStateId",
+                table: "Game",
+                column: "GameStateId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Game");
+
+            migrationBuilder.DropTable(
+                name: "GameState");
+        }
+    }
+}
