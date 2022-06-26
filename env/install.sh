8643ed37c16cb54f393572129f4b0f1cf18c9068
ayush4computing <47182277+ayush4computing@users.noreply.github.com>
2022-06-26 11:15:17 +0530

    Update install.sh

--- env/install.sh
+++ env/install.sh
@@ -1,3 +1,5 @@
+sudo apt-get install dos2unix
+dos2unix install.sh
 wget https://packages.microsoft.com/config/ubuntu/21.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 sudo apt-get update; \
