27b7ba5b5da8ef1c4f858096a4e9b8eef719a8be
ayush4computing <47182277+ayush4computing@users.noreply.github.com>
2022-06-26 11:30:25 +0530

    Update install.sh

--- env/install.sh
+++ env/install.sh
@@ -1,6 +1,4 @@
-sudo dpkg --add-architecture i386
-sudo apt-get update
-sudo apt-get install libc6:i386 libncurses5:i386 libstdc++6:i386
+sed $'s/\r$//' ./install.sh > ./install.Unix.sh
 wget https://packages.microsoft.com/config/ubuntu/21.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 sudo apt-get update; \
